538dbe1406c3b66f9968f7c818e3d30299728c02
amireslamdoust <amireslamdoust@gmail.com>
2020-07-08 19:36:40 +0200

    remove api

--- cypress/integration/convert.spec.js
+++ cypress/integration/convert.spec.js
@@ -9,7 +9,6 @@ describe('test convert works', () => {
     cy.get('[data-cy="price-input-input_price"]').type('50')
     cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
     cy.get('[data-cy="convert-action"]').click()
-    //
     cy.get('[data-cy="USD-balance"]').contains('250')
   })
 
