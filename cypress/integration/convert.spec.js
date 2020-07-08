7918a0344b9469a854843bba8fea82b2d3a2ff15
amireslamdoust <amireslamdoust@gmail.com>
2020-07-08 19:29:42 +0200

    add all test

--- cypress/integration/convert.spec.js
+++ cypress/integration/convert.spec.js
@@ -4,34 +4,34 @@ describe('test convert works', () => {
     cy.contains('Your Balance')
   })
 
-  // it('check number', () => {
-  //   cy.visit('/')
-  //   cy.get('[data-cy="price-input-input_price"]').type('50')
-  //   cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
-  //   cy.get('[data-cy="convert-action"]').click()
-  //   //
-  //   cy.get('[data-cy="USD-balance"]').contains('250')
-  // })
-  //
-  // it('swap action', () => {
-  //   cy.visit('/')
-  //   cy.get('[data-cy="price-input-input_price"]').type('50')
-  //   cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
-  //   cy.get('[data-cy="swap"]').click()
-  //   cy.get('[data-cy="price-input-input_price"]').should('have.value', '50')
-  //   cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
-  //   cy.get('[data-cy="convert-action"]').click()
-  //   cy.get('[data-cy="EUR-balance"]').contains('50')
-  // })
-  //
-  // it('over balance error', () => {
-  //   cy.visit('/')
-  //   cy.get('[data-cy="price-input-input_price"]').type('400')
-  //   cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
-  //   cy.get('[data-cy="price-input-input_price"]').should('have.class', 'border-red-300')
-  //   cy.get('[data-cy="convert-action"]').should('have.class', 'bg-gray-600')
-  //   cy.get('[data-cy="convert-action"]').should('be.disabled')
-  //   cy.get('[data-cy="USD-balance"]').contains('300')
-  //   cy.get('[data-cy="EUR-balance"]').contains('100')
-  // })
+  it('check number', () => {
+    cy.visit('/')
+    cy.get('[data-cy="price-input-input_price"]').type('50')
+    cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
+    cy.get('[data-cy="convert-action"]').click()
+    //
+    cy.get('[data-cy="USD-balance"]').contains('250')
+  })
+
+  it('swap action', () => {
+    cy.visit('/')
+    cy.get('[data-cy="price-input-input_price"]').type('50')
+    cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
+    cy.get('[data-cy="swap"]').click()
+    cy.get('[data-cy="price-input-input_price"]').should('have.value', '50')
+    cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
+    cy.get('[data-cy="convert-action"]').click()
+    cy.get('[data-cy="EUR-balance"]').contains('50')
+  })
+
+  it('over balance error', () => {
+    cy.visit('/')
+    cy.get('[data-cy="price-input-input_price"]').type('400')
+    cy.get('[data-cy="price-input-output_price"]').should('not.have.value', '50')
+    cy.get('[data-cy="price-input-input_price"]').should('have.class', 'border-red-300')
+    cy.get('[data-cy="convert-action"]').should('have.class', 'bg-gray-600')
+    cy.get('[data-cy="convert-action"]').should('be.disabled')
+    cy.get('[data-cy="USD-balance"]').contains('300')
+    cy.get('[data-cy="EUR-balance"]').contains('100')
+  })
 })
